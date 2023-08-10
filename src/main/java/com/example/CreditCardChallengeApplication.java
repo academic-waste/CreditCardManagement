67a56b0e3f2956a57634b27a9ef091ce4ad5a80a
Bernie <995493219@qq.com>
2023-08-10 04:52:46 +0000

    first commit

---
+++ src/main/java/com/example/CreditCardChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardChallengeApplication.class, args);
+	}
+
+}
